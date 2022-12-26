eeca96393e6d27c7d323517b4edd5e3df46c9b13
hamzaMansoor1236 <hamza.mansoor1236@gmail.com>
2022-12-27 03:23:09 +0500

    All request methods PUT POST UPDATE DELETE CATERED

--- index.js
+++ index.js
@@ -1,58 +1,92 @@
-const express= require("express");
+const express = require("express");
+const app = express();
 
-const app= express();
+//middleware used for the  POST method
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+/////////////////////////////////////////////////////////////////////
 
-var bodyParser = require('body-parser')
+//array of users
+const users = [
+  { id: 1, Name: "User1" },
+  { id: 2, Name: "User2" },
+  { id: 3, Name: "User3" },
+];
+/////////////////////////////////////////////////////////////////////
 
-
-
-app.use(bodyParser.json())
-
-
-
-
-
-
-
-
-
-
-const users= [{id:1,Name:"Hamza"},{id:2,Name:"Haroon"},{id:3,Name:"Badar"}]
-
-
-const port =5000;
+//port on which the app will run
+const port = 5000;
+/////////////////////////////////////////////////////////////////////
 
 //route to get all the users
-app.get("/users",(req,res,next)=>{
-
-    res.send(users)
-    next();
-   
-
-})
-
+app.get("/users", (req, res, next) => {
+  res.send(users);
+  next();
+});
+/////////////////////////////////////////////////////////////////////
 
 //route to add new user to the array
-app.post("/users",(req,res)=>{
+app.post("/users", (req, res) => {
+  if (req.body === undefined) {
+    res.send("User cannot be created No body found in the request ");
+  } else {
+    console.log(req.body);
+    users.push(req.body);
+    console.log(users);
+    res.send("User created successfully");
+  }
+});
+/////////////////////////////////////////////////////////////////////
 
+//route to update the specified user
+app.put("/users/:id", (req, res) => {
+  var found = false;
 
-    
+  if (req.body.id !== undefined) {
+    users.map((element) => {
+      if (element.id === parseInt(req.params.id)) {
+        console.log("Match found");
+        element.id = req.body.id;
+        element.Name = req.body.Name;
+        found = true;
+      }
+      console.log(element);
+    });
 
-    if(req.body===undefined){
-        res.send("User cannot be created No body found in the request ")
+    if (found) {
+      console.log(req.body);
+      res.send("User with the id " + req.params.id + "updated successfully");
+    } else {
+      res.send("unable to find the user with id  = " + req.params.id);
     }
-    else{
-        console.log(req.body)
-        users.push(req.body);
-            console.log(users)
-        res.send("User created successfully")
+  } else {
+    res.send("body not found");
+  }
+});
+/////////////////////////////////////////////////////////////////////
+
+//route to delete the specified user
+app.delete("/users/:id", (req, res) => {
+  console.log(req.params);
+  var found = false;
+  users.map((element) => {
+    if (element.id === parseInt(req.params.id)) {
+      console.log("The user with id " + req.params.id + "deleted successfully");
+      found = true;
     }
-    
+  });
+  if (found) {
+    res.send("The user with id " + req.params.id + "deleted successfully");
+  }
+  else{
+    res.send("The user with id " + req.params.id + " does not exist");
 
-    
+  }
+});
+/////////////////////////////////////////////////////////////////////
 
-})
-
-app.listen(port ,()=>{
-    console.log("App running at port 5000   link http://localhost:5000/");
-})
+//app is listening at the port 5000
+app.listen(port, () => {
+  console.log("App running at port 5000   link http://localhost:5000/");
+});
+/////////////////////////////////////////////////////////////////////
